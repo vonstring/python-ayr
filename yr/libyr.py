c35d6d0547944fe8ec0650b38009daed5571825c
idxxx23 <idxxx23@gmail.com>
2014-06-03 13:05:06 +0200

    simplify yr/libyr.py

--- yr/libyr.py
+++ yr/libyr.py
@@ -1,53 +1,42 @@
 #!/usr/bin/env python3
-import sys, json
-from yr.utils import Location, Connect, Cache, Language
+import sys
+import json
+import xmltodict # <~ the only external dependency
+from utils import Connect, Location, Language, YrException
 
 class Yr:
 
     def xmlsource(self):
-        data = Connect(self.location).read()
-        return data
-
-    def xmltosoup(self, xml=None):
-        try:
-            from bs4 import BeautifulSoup
-        except ImportError:
-            sys.stderr.write('import error: from bs4 import BeautifulSoup\n')
-            sys.exit(1)
-        if xml is None:
-            return BeautifulSoup(self.xmlsource())
-        else:
-            return BeautifulSoup(xml)
+        return self.connect.read()
 
     def xmltodict(self, xml=None):
-        try:
-            import xmltodict
-        except ImportError:
-            sys.stderr.write('import error: import xmltodict\n')
-            sys.exit(1)
         if xml is None:
-            return xmltodict.parse(self.xmlsource())
-        else:
-            return xmltodict.parse(xml)
+            xml = self.xmlsource()
+        return xmltodict.parse(xml)
+
+    def dicttoxml(self, dictionary):
+        return xmltodict.unparse(dictionary, pretty=True)
 
     def xmltojson(self, xml=None):
+        if xml is None:
+            xml = self.xmlsource()
         return json.dumps(self.xmltodict(xml), indent=4)
 
-    def now(self, as_json=False): # default is return result as dict ;)
-        soup = self.xmltosoup()
-        xml = str(soup.forecast.tabular.time)
+    def now(self, as_json=True): # default is return result as json ;)
+        xml = self.dicttoxml({'time': self.xmltodict()['weatherdata']['forecast']['tabular']['time'][0]})
         if as_json:
             return self.xmltojson(xml)
         else:
             return self.xmltodict(xml)
 
-    def __init__(self, location_name, language='en'):
+    def __init__(self, location_name, language_name='en'):
         self.location_name = location_name
-        self.language = language
-        self._ = Language(self.language).get_dictionary()
+        self.language_name = language_name
+        self.language = Language(self.language_name)
         self.location = Location(self.location_name, self.language)
+        self.connect = Connect(self.location)
         self.yr_credit = {
-            'text': self._['credit'],
+            'text': self.language.dictionary['credit'],
             'url': 'http://www.yr.no/'
         }
 
@@ -168,3 +157,6 @@ class Yr:
         cache.write(json.dumps(location))
         return location
     """
+
+if __name__ == '__main__':
+    print(Yr('Czech_Republic/Prague/Prague').now())
