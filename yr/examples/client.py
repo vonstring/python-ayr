df95bd4b980db4fc9519abb7732c8764aad1a268
idxxx23 <idxxx23@gmail.com>
2014-06-03 13:34:13 +0200

    simplify yr/examples/client.py

--- yr/examples/client.py
+++ yr/examples/client.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-import os.path, sys
+import sys
+import os.path
 parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 sys.path.append(parent_directory)
 from libyr import Yr
 
-#weather = Yr('Norge/Telemark/Skien/Skien')
-weather = Yr('Czech_Republic/Prague/Prague')
-
-print(weather.now(as_json=True))
+weather = Yr('Norge/Telemark/Skien/Skien')
+print(weather.now())
