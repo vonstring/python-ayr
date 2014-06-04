1cb9e71c73df9a5854050c1418a031a88e3f6367
idxxx23 <idxxx23@gmail.com>
2014-06-04 13:00:37 +0200

    how to get wind_speed ;)

--- yr/examples/client.py
+++ yr/examples/client.py
@@ -4,12 +4,18 @@ from yr.libyr import Yr
 
 weather = Yr('Norge/Telemark/Skien/Skien')
 
-now_json = weather.now(as_json=True)
-print(now_json)
+#now_json = weather.now(as_json=True)
+#print(now_json)
+#
+#for forecast in weather.forecast(as_json=True):
+#    print(forecast)
+#
+#print(weather.xml_source)
+#print(weather.dictionary)
+#print(weather.credit)
 
-for forecast in weather.forecast(as_json=True):
-    print(forecast)
+wind_speed = dict()
+wind_speed['data'] = [{'from': forecast['@from'], 'to': forecast['@to'], 'speed': float(forecast['windSpeed']['@mps'])} for forecast in weather.forecast()]
+wind_speed['credit'] = weather.credit
 
-print(weather.xml_source)
-print(weather.dictionary)
-print(weather.credit)
+print(wind_speed)
