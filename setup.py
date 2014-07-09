a378311981cd7216497c88bea916afe4ef216100
idxxx23 <idxxx23@gmail.com>
2014-07-09 14:15:33 +0200

    mod setup ~> 1.3.0

--- setup.py
+++ setup.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(name='python-yr',
-      version='1.3.0-dev',
-      description='Get the forecast from the norwegian wheather service yr.no in python',
-      author='wckd @ github, idxxx23 @ github',
-      author_email='alexander.l.hansen@gmail.com',
-      url='https://github.com/wckd/python-yr',
-      classifiers=[
-         'Intended Audience :: Developers',
-         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-         'Operating System :: POSIX',
-         'Programming Language :: Python',
-         'Topic :: Internet',
-      ],
-      packages=['yr'],
-      package_data={'yr': ['languages/*.json']},
-      install_requires=['xmltodict'],
-     )
+setup(
+    name = 'python-yr',
+    version = '1.3.0',
+    description = 'Get the forecast from the norwegian wheather service yr.no in python',
+    long_description = 'Get the forecast from the norwegian wheather service yr.no in python',
+    author = 'Alexander Hansen',
+    author_email = 'alexander.l.hansen@gmail.com',
+    maintainer = 'GNU Knight',
+    maintainer_email = 'idxxx23@gmail.com',
+    url = 'https://github.com/wckd/python-yr',
+    packages = ['yr'],
+    package_data = {
+        'yr': [
+            'examples/*.py',
+            'languages/*.json',
+            'locations/*.gz',
+        ]
+    },
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet',
+    ],
+    install_requires = ['xmltodict'], # $$solve$$ ~> /usr/lib/python3.4/distutils/dist.py:260: UserWarning: Unknown distribution option: 'install_requires' warnings.warn(msg)
+)
